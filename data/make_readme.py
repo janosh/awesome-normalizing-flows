a59d33421cc90c651a148cba2f0d2c3d6920d332
Haoyu (Daniel) YANG <yanghaoyu97@outlook.com>
2024-09-22 22:28:56 +0800

    Change `authors` to `list[dict['name' | 'url' | ..., str]]`  (#70)
    * insert shebang and script check
    
    * pre-commit run
    
    * convert to dict of name and url
    
    * convert more to dict
    
    * convert publications
    
    * remove comma
    
    * Update data/packages.yml
    
    Co-authored-by: Meesum Qazalbash <meesumqazalbash@gmail.com>
    
    * fix __main__ loop and define class Author(TypedDict)
    
    TODO author validation
    
    * make script path absolute
    
    * revert reposition of module level vars
    
    * reapply somehow disappeared abs dir change
    
    * raise ValueError on non-https author URLs
    
    fix yaml whitespace
    
    ---------
    
    Co-authored-by: Meesum Qazalbash <meesumqazalbash@gmail.com>
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

--- data/make_readme.py
+++ data/make_readme.py
@@ -1,25 +1,36 @@
+#!/usr/bin/env python3
+
 """Script to generate readme.md from data/*.yml files."""
 
 import datetime
+import os
 import re
-from os.path import dirname
 from typing import TypedDict
 
 import yaml
 
-ROOT = dirname(dirname(__file__))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Author(TypedDict):
+    """An author of a paper or application."""
+
+    name: str
+    url: str | None
+    affiliation: str | None
+    github: str | None
+    orcid: str | None
 
 
 class Item(TypedDict):
     """An item in a readme section like a paper or package."""
 
     title: str
-    authors: str
+    authors: list[Author]
     date: datetime.date
     lang: str
     url: str
     description: str
-    authors_url: str | None
     repo: str | None
     date_added: datetime.date | None
 
@@ -44,7 +55,7 @@ titles = {
 
 def load_items(key: str) -> list[Item]:
     """Load list[Item] from YAML file."""
-    with open(f"{ROOT}/data/{key}.yml", encoding="utf8") as file:
+    with open(f"{ROOT_DIR}/data/{key}.yml", encoding="utf8") as file:
         return yaml.safe_load(file.read())
 
 
@@ -53,10 +64,9 @@ sections: dict[str, Section] = {
     for key in titles  # markdown is set below
 }
 
-
 seen_titles: set[tuple[str, str]] = set()
 required_keys = {"title", "url", "date", "authors", "description"}
-optional_keys = {"authors_url", "lang", "repo", "docs", "date_added", "last_updated"}
+optional_keys = {"lang", "repo", "docs", "date_added", "last_updated"}
 valid_languages = {"PyTorch", "TensorFlow", "JAX", "Julia", "Other"}
 et_al_after = 2
 
@@ -72,7 +82,7 @@ def validate_item(itm: Item, section_title: str) -> None:
     else:
         seen_titles.add((title, section_title))
 
-    if section_title in ("packages", "repos") and itm["lang"] not in valid_languages:
+    if section_title in {"packages", "repos"} and itm["lang"] not in valid_languages:
         errors += [
             f"Invalid lang in {title}: {itm['lang']}, must be one of {valid_languages}"
         ]
@@ -101,87 +111,100 @@ def validate_item(itm: Item, section_title: str) -> None:
         raise ValueError("\n".join(errors))
 
 
-for key, section in sections.items():
-    # Keep lang_names inside sections loop to refill language subsections for each new
-    # section. Used by both repos and Packages. Is a list for order and mutability.
-    lang_names = ["PyTorch", "TensorFlow", "JAX", "Julia", "Other"]
+if __name__ == "__main__":
+    for key, section in sections.items():
+        # Keep lang_names inside sections loop to refill language
+        # subsections for each new section. Used by both repos and Packages.
+        # Is a list for order and mutability.
+        lang_names = ["PyTorch", "TensorFlow", "JAX", "Julia", "Other"]
 
-    # sort first by language with order determined by lang_names (only applies to
-    # Package and repos sections), then by date
-    section["items"].sort(key=lambda x: x["date"], reverse=True)
-    if key in ("packages", "repos"):
-        section["items"].sort(key=lambda itm: lang_names.index(itm["lang"]))
+        # sort first by language with order determined by lang_names (only applies to
+        # Package and repos sections), then by date
+        section["items"].sort(key=lambda x: x["date"], reverse=True)
+        if key in ("packages", "repos"):
+            section["items"].sort(key=lambda itm: lang_names.index(itm["lang"]))
 
-    # add item count after section title
-    section["markdown"] += f" <small>({len(section['items'])})</small>\n\n"
+        # add item count after section title
+        section["markdown"] += f" <small>({len(section['items'])})</small>\n\n"
 
-    for itm in section["items"]:
-        if (lang := itm.get("lang")) in lang_names:
-            lang_names.remove(lang)
-            # print language subsection title if this is the first item with that lang
-            section["markdown"] += (
-                f'<br>\n\n### <img src="assets/{lang.lower()}.svg" alt="{lang}" '
-                f'height="20px"> &nbsp;{lang} {key.title()}\n\n'
+        for itm in section["items"]:
+            if (lang := itm.get("lang")) in lang_names:
+                lang_names.remove(lang)
+                # print language subsection title if this is the first item
+                # with that language
+                section["markdown"] += (
+                    f'<br>\n\n### <img src="assets/{lang.lower()}.svg" alt="{lang}" '
+                    f'height="20px"> &nbsp;{lang} {key.title()}\n\n'
+                )
+
+            validate_item(itm, section["title"])
+
+            authors = itm["authors"]
+            date = itm["date"]
+            description = itm["description"]
+            title = itm["title"]
+            url = itm["url"]
+
+            if key in ("publications", "applications"):
+                # only show people's last name for papers
+                authors = [
+                    auth | {"name": auth["name"].split(" ")[-1]} for auth in authors
+                ]
+
+            def auth_str(auth: Author) -> str:
+                """Return a markdown string for an author."""
+                auth_str = auth["name"]
+                if url := auth.get("url"):
+                    if not url.startswith("https://"):
+                        raise ValueError(
+                            f"Invalid author {url=}, must start with https://"
+                        )
+                    auth_str = f"[{auth_str}]({url})"
+                return auth_str
+
+            authors_str = ", ".join(map(auth_str, authors[:et_al_after]))
+            if len(authors) > et_al_after:
+                authors_str += " et al."
+
+            md_str = f"1. {date} - [{title}]({url}) by {authors_str}"
+
+            if key in ("packages", "repos") and url.startswith("https://github.com"):
+                gh_login, repo_name = url.split("/")[3:5]
+                md_str += (
+                    f'\n&ensp;\n<img src="https://img.shields.io/github/stars/'
+                    f'{gh_login}/{repo_name}" alt="GitHub repo stars"'
+                    ' valign="middle" />'
+                )
+
+            md_str += "<br>\n   " + description.removesuffix("\n")
+            if docs := itm.get("docs"):
+                md_str += f" [[Docs]({docs})]"
+            if repo := itm.get("repo"):
+                md_str += f" [[Code]({repo})]"
+
+            section["markdown"] += md_str + "\n\n"
+
+    with open(f"{ROOT_DIR}/readme.md", "r+", encoding="utf8") as file:
+        readme = file.read()
+
+        for section in sections.values():
+            # look ahead without matching
+            section_start_pat = f"(?<={section['title']})"
+            # look behind without matching
+            next_section_pat = "(?=<br>\n\n## )"
+
+            # match everything up to next heading
+            readme = re.sub(
+                rf"{section_start_pat}[\s\S]+?\n\n{next_section_pat}",
+                section["markdown"],
+                readme,
             )
 
-        validate_item(itm, section["title"])
+        file.seek(0)
+        file.write(readme)
+        file.truncate()
 
-        authors = itm["authors"]
-        date = itm["date"]
-        description = itm["description"]
-        title = itm["title"]
-        url = itm["url"]
-
-        author_list = authors.split(", ")
-        if key in ("publications", "applications"):
-            # only show people's last name for papers
-            author_list = [author.split(" ")[-1] for author in author_list]
-        authors = ", ".join(author_list[:et_al_after])
-        if len(author_list) > et_al_after:
-            authors += " et al."
-
-        if authors_url := itm.get("authors_url"):
-            authors = f"[{authors}]({authors_url})"
-
-        md_str = f"1. {date} - [{title}]({url}) by {authors}"
-
-        if key in ("packages", "repos") and url.startswith("https://github.com"):
-            gh_login, repo_name = url.split("/")[3:5]
-            md_str += (
-                f'\n&ensp;\n<img src="https://img.shields.io/github/stars/'
-                f'{gh_login}/{repo_name}" alt="GitHub repo stars" valign="middle" />'
-            )
-
-        md_str += "<br>\n   " + description.removesuffix("\n")
-        if docs := itm.get("docs"):
-            md_str += f" [[Docs]({docs})]"
-        if repo := itm.get("repo"):
-            md_str += f" [[Code]({repo})]"
-
-        section["markdown"] += md_str + "\n\n"
-
-
-with open(f"{ROOT}/readme.md", "r+", encoding="utf8") as file:
-    readme = file.read()
-
-    for section in sections.values():
-        # look ahead without matching
-        section_start_pat = f"(?<={section['title']})"
-        # look behind without matching
-        next_section_pat = "(?=<br>\n\n## )"
-
-        # match everything up to next heading
-        readme = re.sub(
-            rf"{section_start_pat}[\s\S]+?\n\n{next_section_pat}",
-            section["markdown"],
-            readme,
-        )
-
-    file.seek(0)
-    file.write(readme)
-    file.truncate()
-
-section_counts = "\n".join(
-    f"- {key}: {len(sec['items'])}" for key, sec in sections.items()
-)
-print(f"finished writing {len(seen_titles)} items to readme:\n{section_counts}")  # noqa: T201
+    section_counts = "\n".join(
+        f"- {key}: {len(sec['items'])}" for key, sec in sections.items()
+    )
+    print(f"finished writing {len(seen_titles)} items to readme:\n{section_counts}")  # noqa: T201
